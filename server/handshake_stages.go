9a391d454d63dc3c6c12603578ee529d905c3b9a
Smile <smileuwui@proton.me>
2026-05-02 19:33:35 +0300

    Added error and informational logging on the server side

--- server/handshake_stages.go
+++ server/handshake_stages.go
@@ -15,28 +15,33 @@ import (
 )
 
 func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err error) {
+	c.logger.Info("Starting handshake stage 1 for client %s", c.addr)
+
 	err = c.conn.SetDeadline(time.Now().Add(15 * time.Second))
 	if err != nil {
+		c.logger.Error("Failed to set deadline for client %s: %v", c.addr, err)
 		return err
 	}
-	c.conn.SetDeadline(time.Now().Add(15 * time.Second))
 	c.sessionRecvKey = initPassword[:]
 	c.sessionSentKey = initPassword[:]
 
 	usernamePacket, err := c.readPacket()
 	if err != nil {
+		c.logger.Error("Failed to read username packet from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	err = usernamePacket.DecodeAndDecrypt(initPassword[:], false)
 	if err != nil {
+		c.logger.Error("Failed to decrypt username packet from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	timestampBytes, err := usernamePacket.GetSlicePlainData(16, 24)
 	if err != nil {
+		c.logger.Error("Failed to get timestamp from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
@@ -44,29 +49,36 @@ func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err
 	timestamp := binary.BigEndian.Uint64(timestampBytes)
 	currentTime := time.Now().Unix()
 	if timestamp > math.MaxInt64 {
+		c.logger.Error("Timestamp overflow from client %s: %d", c.addr, timestamp)
 		return fmt.Errorf("timestamp overflow")
 	}
 	timeDiff := currentTime - int64(timestamp)
 
 	if timeDiff > 5 {
+		c.logger.Error("Invalid timestamp from client %s: diff=%d, current=%d, client=%d", c.addr, timeDiff, currentTime, timestamp)
 		c.conn.Close()
 		return fmt.Errorf("invalid timestamp")
 	}
 
 	username, err := usernamePacket.GetSlicePlainData(0, 16)
 	if err != nil {
+		c.logger.Error("Failed to get username from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
+	c.logger.Info("Client %s attempting authentication with username: %x", c.addr, username)
+
 	user := users.GetUser([16]byte(username))
 	if user == nil {
+		c.logger.Error("User not found for client %s: username=%x", c.addr, username)
 		c.conn.Close()
 		return fmt.Errorf("user not found (username: %x)", username)
 	}
 
 	salt, err := crypto.RandomBytes(32)
 	if err != nil {
+		c.logger.Error("Failed to generate salt for client %s: %v", c.addr, err)
 		return err
 	}
 
@@ -74,12 +86,13 @@ func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err
 	saltPacket.AddData(salt)
 	err = saltPacket.PackageAssembly(initPassword[:], []byte{}, []byte{}, false, false)
 	if err != nil {
+		c.logger.Error("Failed to package salt packet for client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	if _, err = c.conn.Write(saltPacket.GetRawData()); err != nil {
-
+		c.logger.Error("Failed to send salt packet to client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
@@ -88,6 +101,7 @@ func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err
 	password := user.GetPassword()
 	firstSalt, err := saltPacket.GetSlicePlainData(0, 16)
 	if err != nil {
+		c.logger.Error("Failed to get first salt for client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
@@ -99,6 +113,7 @@ func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err
 	sessionSentKeyHasher := sha256.New()
 	secondSalt, err := saltPacket.GetSlicePlainData(16, 32)
 	if err != nil {
+		c.logger.Error("Failed to get second salt for client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
@@ -107,42 +122,53 @@ func (c *Client) handshakeStage1(initPassword [32]byte, users *users.Users) (err
 	sessionSentKeyHasher.Write(secondSalt)
 	c.sessionSentKey = sessionSentKeyHasher.Sum(nil)
 
+	c.logger.Info("Handshake stage 1 completed for client %s", c.addr)
 	return nil
 }
 
 func (c *Client) handshakeStage2(clientIP *net.IP) (err error) {
+	c.logger.Info("Starting handshake stage 2 for client %s, assigned IP: %s", c.addr, clientIP.String())
+
 	packet, err := c.readPacket()
 	if err != nil {
+		c.logger.Error("Failed to read stage 2 packet from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	err = packet.DecodeAndDecrypt(c.sessionRecvKey, false)
 	if err != nil {
+		c.logger.Error("Failed to decrypt stage 2 packet from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	confirmationByte, err := packet.GetSlicePlainData(0, 1)
 	if err != nil {
+		c.logger.Error("Failed to get confirmation byte from client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	if confirmationByte[0] != 0xFF {
+		c.logger.Error("Client %s rejected the connection (confirmation byte: %x)", c.addr, confirmationByte[0])
 		c.conn.Close()
 		return fmt.Errorf("the client rejected the connection")
 	}
+
+	c.logger.Info("Client %s confirmed connection", c.addr)
 
 	curve := ecdh.X25519()
 
 	publicClientKey, err := curve.NewPublicKey(packet.GetPublicKey())
 	if err != nil {
+		c.logger.Error("Failed to parse client's public key for %s: %v", c.addr, err)
 		return err
 	}
 
 	privateServerKey, err := curve.GenerateKey(rand.Reader)
 	if err != nil {
+		c.logger.Error("Failed to generate server private key for %s: %v", c.addr, err)
 		return err
 	}
 
@@ -152,24 +178,28 @@ func (c *Client) handshakeStage2(clientIP *net.IP) (err error) {
 	ipPacket.AddData(clientIP.To4())
 	err = ipPacket.PackageAssembly(c.sessionSentKey, []byte{}, publicServerKey.Bytes(), false, true)
 	if err != nil {
+		c.logger.Error("Failed to package IP packet for client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	if _, err = c.conn.Write(ipPacket.GetRawData()); err != nil {
-
+		c.logger.Error("Failed to send IP assignment to client %s: %v", c.addr, err)
 		c.conn.Close()
 		return err
 	}
 
 	c.localIP = clientIP
+	c.logger.Info("Assigned IP %s to client %s", clientIP.String(), c.addr)
 
 	secret, err := privateServerKey.ECDH(publicClientKey)
 	if err != nil {
+		c.logger.Error("Failed to compute shared secret for client %s: %v", c.addr, err)
 		return err
 	}
 	c.computeNextSessionRecvKey(secret)
 	c.computeNextSessionSentKey(secret)
 
+	c.logger.Info("Handshake stage 2 completed for client %s", c.addr)
 	return nil
 }
--- server/server.go
+++ server/server.go
@@ -51,8 +51,10 @@ func NewServer(cfg *config.Config, usersDB *users.Users, logger *logger.Logger) 
 func (s *Server) Start() error {
 	addr := fmt.Sprintf("%s:%d", s.config.Host, s.config.Port)
 
+	s.logger.Info("Starting the server")
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
+		s.logger.Error("Server startup error: %v", err)
 		return fmt.Errorf("failed to start TCP server: %w", err)
 	}
 
@@ -66,6 +68,7 @@ func (s *Server) Start() error {
 	)
 
 	if err != nil {
+		s.logger.Error("Error create tunnel: %v", err)
 		return err
 	}
 
@@ -75,6 +78,8 @@ func (s *Server) Start() error {
 	if err != nil {
 		return err
 	}
+
+	s.logger.Info("Starting the main VPN operation cycle")
 
 	go s.acceptConnections()
 
@@ -111,6 +116,8 @@ func (s *Server) acceptConnections() {
 				continue
 			}
 
+			s.logger.Info("New connection from %s", conn.RemoteAddr().String())
+
 			s.wg.Add(1)
 			s.incrementClientCount()
 
@@ -145,22 +152,29 @@ func (s *Server) handleConnection(conn net.Conn) {
 		maxPacketLength: 4096,
 	}
 
+	clientAddr := connTCP.RemoteAddr().String()
+
+	s.logger.Info("The handshake process with client %s has begun", clientAddr)
 	err := client.handshakeStage1(s.config.InitPassword, s.users)
 	if err != nil {
+		s.logger.Error("Error during the first stage of the handshake with client %s: %v", clientAddr, err)
 		return
 	}
 
 	clientIP := s.ipPool.AcquireIP()
 	err = client.handshakeStage2(&clientIP)
 	if err != nil {
+		s.logger.Error("Error during the second stage of the handshake with client %s: %v", clientAddr, err)
 		s.ipPool.ReleaseIP(clientIP)
 		return
 	}
+	s.logger.Info("The handshake process with client %s is complete", clientAddr)
 
 	s.clients[clientIP.String()] = client
 
 	err = conn.SetDeadline(time.Time{})
 	if err != nil {
+		s.logger.Error("Error setting the deadline for client %s: %v", clientAddr, err)
 		return
 	}
 
@@ -168,10 +182,12 @@ func (s *Server) handleConnection(conn net.Conn) {
 }
 
 func (s *Server) tunnelReader() {
+	s.logger.Info("Tunnel reader started")
 	for {
 		rawPacket := make([]byte, 65535)
 		n, err := s.tunnel.Read(rawPacket)
 		if err != nil {
+			s.logger.Error("Tunnel read error: %v", err)
 			continue
 		}
 		rawPacket = rawPacket[:n]
@@ -180,9 +196,10 @@ func (s *Server) tunnelReader() {
 		}
 
 		dstIP := rawPacket[16:20]
+		ipStr := fmt.Sprintf("%d.%d.%d.%d", dstIP[0], dstIP[1], dstIP[2], dstIP[3])
 
 		s.mu.Lock()
-		client, ok := s.clients[fmt.Sprintf("%d.%d.%d.%d", dstIP[0], dstIP[1], dstIP[2], dstIP[3])]
+		client, ok := s.clients[ipStr]
 		s.mu.Unlock()
 		if !ok {
 			continue
@@ -194,29 +211,35 @@ func (s *Server) tunnelReader() {
 
 		salt, err := crypto.RandomBytes(8)
 		if err != nil {
+			s.logger.Error("Failed to generate salt for client %s: %v", client.addr, err)
 			continue
 		}
 
 		packet := packets.NewPlainPacket()
 		packet.AddData(rawPacket)
 
-		if client.countSent >= uint32(math.Pow(2, 16)) || time.Since(client.lastRoundECDH) >= 4*time.Minute {
+		needsECDH := client.countSent >= uint32(math.Pow(2, 16)) || time.Since(client.lastRoundECDH) >= 4*time.Minute
+		if needsECDH {
+			s.logger.Debug("Initiating ECDH rekey for client %s (countSent=%d, lastRoundECDH=%v)",
+				client.addr, client.countSent, time.Since(client.lastRoundECDH))
+
 			curve := ecdh.X25519()
 			privateKey, err := curve.GenerateKey(rand.Reader)
 			if err != nil {
+				s.logger.Error("Failed to generate ephemeral key for client %s: %v", client.addr, err)
 				return
 			}
 
 			client.ephemeralPrivateServerKey = privateKey
 			err = packet.PackageAssembly(client.sessionSentKey, salt, privateKey.PublicKey().Bytes(), false, true)
-
 			if err != nil {
+				s.logger.Error("Failed to package packet with ECDH for client %s: %v", client.addr, err)
 				continue
 			}
 		} else {
 			err = packet.PackageAssembly(client.sessionSentKey, salt, []byte{}, false, false)
-
 			if err != nil {
+				s.logger.Error("Failed to package packet for client %s: %v", client.addr, err)
 				continue
 			}
 		}
@@ -224,14 +247,17 @@ func (s *Server) tunnelReader() {
 		_, err = client.conn.Write(packet.GetRawData())
 		if err != nil {
 			if errors.Is(err, net.ErrClosed) {
+				s.logger.Info("Client %s connection closed, releasing IP %s", client.addr, client.localIP.String())
 				s.ipPool.ReleaseIP(*client.localIP)
 				delete(s.clients, client.localIP.String())
 				continue
 			}
+			s.logger.Error("Failed to write packet to client %s: %v", client.addr, err)
 			continue
 		}
 
 		if packet.GetEcdhFlag() {
+			s.logger.Debug("ECDH flag set for client %s, creating lock", client.addr)
 			client.roundECDHLock = make(chan struct{}, 1)
 		}
 
@@ -241,31 +267,40 @@ func (s *Server) tunnelReader() {
 }
 
 func (s *Server) handleClient(client *Client) {
-	defer client.conn.Close()
+	s.logger.Info("Starting to handle client %s (IP: %s)", client.addr, client.localIP.String())
+	defer func() {
+		s.logger.Info("Stopping to handle client %s (IP: %s)", client.addr, client.localIP.String())
+		client.conn.Close()
+	}()
 
 	for {
 		select {
 		case <-s.stopCh:
+			s.logger.Info("Stop signal received, stopping handler for client %s", client.addr)
 			return
 		default:
 			packet, err := client.readPacket()
 			if err != nil {
 				if errors.Is(err, net.ErrClosed) {
+					s.logger.Info("Client %s connection closed (net.ErrClosed), releasing IP %s", client.addr, client.localIP.String())
 					s.ipPool.ReleaseIP(*client.localIP)
 					delete(s.clients, client.localIP.String())
 					return
 				}
 
 				if err.Error() == "EOF" {
+					s.logger.Info("Client %s disconnected (EOF), releasing IP %s", client.addr, client.localIP.String())
 					s.ipPool.ReleaseIP(*client.localIP)
 					delete(s.clients, client.localIP.String())
 					return
 				}
+				s.logger.Error("Failed to read packet from client %s: %v", client.addr, err)
 				continue
 			}
-			err = packet.DecodeAndDecrypt(client.sessionRecvKey, true)
 
+			err = packet.DecodeAndDecrypt(client.sessionRecvKey, true)
 			if err != nil {
+				s.logger.Error("Failed to decrypt packet from client %s: %v", client.addr, err)
 				continue
 			}
 
@@ -273,14 +308,18 @@ func (s *Server) handleClient(client *Client) {
 			client.computeNextSessionRecvKey(packet.GetSalt())
 
 			if packet.GetEcdhFlag() && client.ephemeralPrivateServerKey != nil {
+				s.logger.Debug("Processing ECDH rekey from client %s", client.addr)
+
 				curve := ecdh.X25519()
 				clientPublicKey, err := curve.NewPublicKey(packet.GetPublicKey())
 				if err != nil {
+					s.logger.Error("Failed to parse client public key for %s: %v", client.addr, err)
 					return
 				}
 
 				secret, err := client.ephemeralPrivateServerKey.ECDH(clientPublicKey)
 				if err != nil {
+					s.logger.Error("Failed to compute shared secret for client %s: %v", client.addr, err)
 					return
 				}
 				client.ephemeralPrivateServerKey = nil
@@ -292,6 +331,8 @@ func (s *Server) handleClient(client *Client) {
 				client.computeNextSessionSentKey(secret)
 				client.computeNextSessionRecvKey(secret)
 				close(client.roundECDHLock)
+
+				s.logger.Info("ECDH rekey completed for client %s", client.addr)
 			}
 
 			client.mu.Lock()
@@ -307,14 +348,18 @@ func (s *Server) handleClient(client *Client) {
 
 			ipSrc, err := packet.GetSlicePlainData(12, 16)
 			if err != nil {
+				s.logger.Error("Failed to get source IP from packet for client %s: %v", client.addr, err)
 				continue
 			}
-			if fmt.Sprintf("%d.%d.%d.%d", ipSrc[0], ipSrc[1], ipSrc[2], ipSrc[3]) != client.localIP.String() {
+
+			srcIP := fmt.Sprintf("%d.%d.%d.%d", ipSrc[0], ipSrc[1], ipSrc[2], ipSrc[3])
+			if srcIP != client.localIP.String() {
 				continue
 			}
 
 			_, err = s.tunnel.Write(packet.GetPlainData())
 			if err != nil {
+				s.logger.Error("Failed to write packet to tunnel for client %s: %v", client.addr, err)
 				continue
 			}
 		}
